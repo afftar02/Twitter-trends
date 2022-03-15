3745469499efdc8c04cf1c6f6cf928f9c1108013
Maksim Saikovski <saykovskiy.maks03@gmail.com>
2022-03-15 09:04:14 +0300

    States parser added

---
+++ Twitter trends/Services/Parsers/StatesParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Twitter_trends.Services.Parsers
+{
+    public static class StatesParser
+    {
+        public static void Parse(string path)
+        {
+            string jsonString = File.ReadAllText(path);
+            jsonString = Regex.Replace(jsonString, @"[^\S\r\n]?[\[\]\:]?", "").Replace("{", "").Replace("}", "").Replace(",", " ");
+            jsonString = Regex.Replace(jsonString, @"[\r\n]+", "\n").Replace("\"", "").Trim();
+
+            string state_name = jsonString.Split('\n')[0];
+            string state_info = "";
+            jsonString = jsonString.Remove(0, 3);
+
+           
+            //Console.WriteLine(jsonString);
+        }
+    }
+}
